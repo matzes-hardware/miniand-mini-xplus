d97bf326e51f7039f26de2f53c8ebe0530dadac5
Matthias <da-kaiser@der-ball-ist-rund.net>
2013-02-04 00:36:49 +0100

    seems to boot

--- linux-sunxi/copy.sh
+++ linux-sunxi/copy.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 echo "kernel ..."
+umount /dev/sdb1
 mount /dev/sdb1 /mnt
 cp arch/arm/boot/uImage /mnt -v
 sleep 2
 umount /mnt
 
 echo "modules ..."
+umount /dev/sdb2
 mount /dev/sdb2 /mnt
 rm /mnt/lib/modules/* -fR
 cp output/lib/modules/* /mnt/lib/modules/ -vR
