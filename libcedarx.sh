8242086da0d45a04ef121d8659a1aabe8f1909ae
Matthias <da-kaiser@der-ball-ist-rund.net>
2013-02-03 19:43:54 +0100

    armel library

--- libcedarx.sh
+++ libcedarx.sh
@@ -1,7 +1,8 @@
 apt-get install -y tool gcc-4.4-arm-linux-gnueabi g++-4.4-arm-linux-gnueabi cpp-4.4-arm-linux-gnueabi
 git clone https://github.com/willswang/libcedarx
 cd libcedarx
+wget https://github.com/linux-sunxi/cedarx-libs/blob/master/libcedarv/linux-armel/libvecore/libvecore.so?raw=true -O libvecore.so
 ./autogen.sh
-./configure --host=arm-linux-gnueabi --prefix=/home/code/Mini-Xplus-Firmware/libcedarx
+./configure --host=arm-linux-gnueabi --prefix=/usr
 make
 make install
